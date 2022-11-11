53884bce6f53979ab3cb510f7e1d6d929847fdad
fbtty <115198968+fbtty@users.noreply.github.com>
2022-11-11 09:54:00 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 input.onButtonPressed(Button.A, function () {
-    for (let index = 0; index <= 5; index++) {
-        led.plot(index, 25 % 5)
+    for (let index = 0; index <= 25; index++) {
+        led.plot(index / 4, index % 5)
         basic.pause(200)
     }
 })
